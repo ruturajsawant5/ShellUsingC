b7e6d8837a0f5c27586fa520a1f26f5f9a54e93c
Ruturaj Sawant <ruturajsawant5@gmail.com>
2025-11-06 11:31:51 +0000

    take cmd input

--- src/main.c
+++ src/main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define INPUT_CMD_LEN 1024
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
+
+  char input_cmd[INPUT_CMD_LEN];
+  fgets(input_cmd, INPUT_CMD_LEN, stdin);
+
+  //remove trailing spaces
+  input_cmd[strcspn(input_cmd, "\n")] = '\0';
+
+  printf("%s: command not found\n", input_cmd);
 
   return 0;
 }
