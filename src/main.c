329d438ad1287c98642b7f2620eebaf3d5630397
Ruturaj Sawant <ruturajsawant5@gmail.com>
2025-11-10 16:20:50 +0000

    quotes fix

--- src/main.c
+++ src/main.c
@@ -90,7 +90,7 @@ char** parse_cmd(const char* cmd, int* out_count)
                 // quoted section
                 p++; // skip opening "
                 while (*p && *p != '\"') {
-                    if(*p == '\\')
+                    if(p[0] == '\\' && p[1] == '\"')
                       p++;
                     buffer[bi++] = *p++;
                 }
